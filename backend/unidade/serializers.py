37ce979e264095bd5f37fb1399280153b37aa05c
Kellyklo <kellykloti@gmail.com>
2026-09-14 10:34:08 -0300

    refactor(backend): mudança de framework

--- backend/unidade/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import UnidadeModels
-
-#Transforma os dados HTTP para uso no Python e no Django.
-class UnidadeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UnidadeModels
-        fields = '__all__'
