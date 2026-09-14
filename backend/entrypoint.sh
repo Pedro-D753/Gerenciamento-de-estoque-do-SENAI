84402b4c4fc86a24104aae2233cfc9e4b97b72fb
Kellyklo <kellykloti@gmail.com>
2026-09-14 10:34:08 -0300

    refactor(backend): mudança de framework

--- backend/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#aplica a migrate do Django de forma silenciosa e sem o Django pedir qualquer tipo de input
-python manage.py migrate --noinput --verbosity 0
-
-#devolver o controle para o Dockerfile após a execução desse script
-exec "$@"
--- backend/unidade/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-
-# Registre seus modelos aqui.
